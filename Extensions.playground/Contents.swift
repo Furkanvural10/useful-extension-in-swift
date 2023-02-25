3353e6c7f76b098c9131410832aeb3285d14244e
Furkan Vural <furkanvural52@gmail.com>
2023-02-25 22:20:09 +0300

    Added counting words in a string extension

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -10,3 +10,15 @@ func getName(name: String?){
         // handle
     }
 }
+
+
+// Counting words in a string - **hacking with swift
+extension String {
+    var wordCount: Int {
+        let regex = try? NSRegularExpression(pattern: "\\w+")
+        return regex?.numberOfMatches(in: self, range: NSRange(location: 0, length: self.utf16.count)) ?? 0
+    }
+}
+
+let sentences = "lorem ipsum dolor sit amet"
+let countWord = sentences.wordCount
