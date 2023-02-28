4221d8e2b9c9a7a9ca456063430677fdea85986b
Furkan Vural <furkanvural52@gmail.com>
2023-02-28 23:59:34 +0300

    Added conver to metre extension

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -22,3 +22,13 @@ extension String {
 
 let sentences = "lorem ipsum dolor sit amet"
 let countWord = sentences.wordCount
+
+// convert to meter extension
+
+extension Double {
+    var km: Double {return self * 1000.0}
+    var m: Double {return self}
+    var cm: Double {return self / 100.0}
+    var mm: Double {return self / 1000.0}
+    
+}
