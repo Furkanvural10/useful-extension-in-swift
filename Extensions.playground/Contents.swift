59cbff5e9aaa9b1e35f38b6c3a6b540c049039b0
Furkan Vural <furkanvural52@gmail.com>
2023-03-20 12:38:38 +0300

    Added UIColor with HEX Color extension

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -32,3 +32,17 @@ extension Double {
     var mm: Double {return self / 1000.0}
     
 }
+
+// MARK: - UICOLOR With HEX Color
+
+extension UIColor {
+    convenience init(hex: Int, alpha: CGFloat = 1.0) {
+            self.init(
+                red: CGFloat((hex >> 16) & 0xff) / 255,
+                green:  CGFloat((hex >> 08) & 0xff) / 255,
+                blue: CGFloat((hex >> 00) & 0xff) / 255,
+                alpha: alpha
+            )
+    }
+
+}
